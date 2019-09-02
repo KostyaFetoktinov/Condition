de44c7e23fe6ad8433a3814a8843e10eedd8767c
КОСТЯ <@LAPTOP-1MLQP76Q>
2019-09-02 15:13:36 +0300

    case 5

--- CycleOperators/DoWhileApp/Program.cs
+++ CycleOperators/DoWhileApp/Program.cs
@@ -45,6 +45,8 @@ namespace DoWhileApp
                             break;
                         case 4:
                             break;
+                        case 5:
+                            break;
                             
                     }
 
